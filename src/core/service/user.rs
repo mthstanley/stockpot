042b2d09911329a08910537c94f98e44a2d2ee0d
Matthew Stanley <stanley.t.matthew@gmail.com>
2023-12-10 17:19:10 -0500

    simplifies import

--- src/core/service/user.rs
+++ src/core/service/user.rs
@@ -1,11 +1,11 @@
-use crate::core::port;
+use crate::core::{domain, port};
 
 pub struct UserService {
     user_repository: Box<dyn port::UserRepository>,
 }
 
 impl port::UserService for UserService {
-    fn get_user(&self, id: u64) -> anyhow::Result<crate::core::domain::User> {
+    fn get_user(&self, id: u64) -> anyhow::Result<domain::User> {
         self.user_repository.get_user_by_id(id)
     }
 }
