595f3040ee52b595b36e4e4140160172aa92fb0d
Matthew Stanley <stanley.t.matthew@gmail.com>
2024-12-30 12:50:27 -0500

    adds recipe timing and yield to recipe card

--- ui/src/components/recipe/RecipeCard.tsx
+++ ui/src/components/recipe/RecipeCard.tsx
@@ -12,12 +12,36 @@ const RecipeCard = ({
 }) => {
   const Heading = headingLevel;
   return (
-    <>
+    <section>
       <Heading>
         <Link to={`/recipes/${recipe.id}`}>{recipe.title}</Link>
       </Heading>
+      <dl>
+        {recipe.prepTime !== null && (
+          <>
+            <dt>Prep Time</dt>
+            <dd>{recipe.prepTime} seconds</dd>
+          </>
+        )}
+        {recipe.cookTime !== null && (
+          <>
+            <dt>Cook Time</dt>
+            <dd>{recipe.cookTime} seconds</dd>
+          </>
+        )}
+        {recipe.inactiveTime !== null && (
+          <>
+            <dt>Inactive Time</dt>
+            <dd>{recipe.inactiveTime} seconds</dd>
+          </>
+        )}
+        <dt>Yields</dt>
+        <dd>
+          {recipe.yieldQuantity} {recipe.yieldUnits}
+        </dd>
+      </dl>
       <p>{recipe.description}</p>
-    </>
+    </section>
   );
 };
 
