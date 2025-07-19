71349bf23f9f4b9e29b21785aef47a0b03f1a9c8
Matthew Stanley <stanley.t.matthew@gmail.com>
2025-07-19 11:32:09 -0400

    fixes navbar height and main content offset to account for navbar

--- ui/src/components/navigation/Navbar.tsx
+++ ui/src/components/navigation/Navbar.tsx
@@ -7,34 +7,38 @@ const Navbar = () => {
   return (
     <nav>
       <div className="content">
-        <h2 className="brand">Stockpot</h2>
-        <ul>
-          <li>
-            <NavLink to="/">Home</NavLink>
-          </li>
-          <li>
-            <NavLink to="recipes">Recipes</NavLink>
-          </li>
-          {auth.user ? (
-            <>
-              <li>
-                <NavLink to="users">Profile</NavLink>
-              </li>
-              <li>
-                <NavLink to="users/signout">Sign-out</NavLink>
-              </li>
-            </>
-          ) : (
-            <>
-              <li>
-                <NavLink to="users/signin">Sign-in</NavLink>
-              </li>
-              <li>
-                <NavLink to="users/signup">Sign-up</NavLink>
-              </li>
-            </>
-          )}
-        </ul>
+        <div className="brand">
+          <h2>Stockpot</h2>
+        </div>
+        <div className="links">
+          <ul>
+            <li>
+              <NavLink to="/">Home</NavLink>
+            </li>
+            <li>
+              <NavLink to="recipes">Recipes</NavLink>
+            </li>
+            {auth.user ? (
+              <>
+                <li>
+                  <NavLink to="users">Profile</NavLink>
+                </li>
+                <li>
+                  <NavLink to="users/signout">Sign-out</NavLink>
+                </li>
+              </>
+            ) : (
+              <>
+                <li>
+                  <NavLink to="users/signin">Sign-in</NavLink>
+                </li>
+                <li>
+                  <NavLink to="users/signup">Sign-up</NavLink>
+                </li>
+              </>
+            )}
+          </ul>
+        </div>
       </div>
     </nav>
   );
